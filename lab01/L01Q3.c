25ef26c1f394e0319da72f93db4d076240d52d78
2611710019-create <2611710019@tni.ac.th>
2026-07-06 12:22:34 +0700

    lab01 submission

---
+++ lab01/L01Q3.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+#include <stdlib.h> 
+
+ 
+
+int main() 
+
+{ 
+
+    printf("Hello!\n" "my name is kritthanach khongong\n" "Nice to meet you!\n"); 
+
+
+    return 0; 
+
+} 

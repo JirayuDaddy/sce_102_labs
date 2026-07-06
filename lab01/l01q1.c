47717291f9f3c72b0c891396012fc0ba2d2d529e
2611740024 <2611740024@tni.ac.th>
2026-07-06 11:54:49 +0700

    lab01 submission

---
+++ lab01/l01q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+int main() 
+
+{ 
+
+    printf("Hello my world!\n"); 
+
+    printf("MAGIC!\n"); 
+
+    return 0; 
+
+}
---
+++ lab01/l01q3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("hello\nMy name is peerakarn \nnicetomeetyou");
+    return 0;
+}
+

9461f774226a1c7747857720714f597af8a0df9a
unknown <2611710076@tni.ac.th>
2026-07-06 11:46:13 +0700

    lab01 submission

---
+++ lab01/L01Q2.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main() 
+{ 
+    printf("MAGIC! "); 
+    printf("Hello my world!\n"); 
+    return 0; 
+} 
---
+++ lab01/L01Q3.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+int main() 
+{ 
+    printf("Hello!\nMy name is Punnaphat Chinsiriratkun. \nNice to meet you!\nCodeding is MAGIC!"); 
+    return 0; 
+} 

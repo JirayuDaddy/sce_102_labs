c8c0f5ce377cd3d75fcc82d8c431e59c34a2d12f
unknown <2611710142@tni.ac.th>
2026-07-12 12:16:33 +0700

    lab02 submisson

--- lab02/L02Q2.c
+++ lab02/L02Q2.c
@@ -7,7 +7,7 @@ int main(){
     float MILLI_RATE = 10;
     float METER_RATE = 100;
     float INCH_RATE = 2.54;
-    printf("Enter length in centimeter:");
+    printf("Enter length in centimeter: ");
     scanf("%f", &enter_centi);
     printf("\n%.2f centimeter = %.2f millimeters, %.2f meters, and %.2f inches", enter_centi, enter_centi * MILLI_RATE, enter_centi / METER_RATE, enter_centi / INCH_RATE);
 

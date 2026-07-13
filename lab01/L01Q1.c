d7e947e1a32086c27e095c88171cea69533248f7
2611710183 <2611710183@tni.ac.th>
2026-07-13 12:27:15 +0700

    lab02 submission

--- lab01/L01Q1.c
+++ lab01/L01Q1.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,3 +8,22 @@ int main()
     printf("MAGIC!\n");
     return 0;
 }
+=======
+#include <stdio.h> 
+
+#include <stdlib.h> 
+
+ 
+
+int main() 
+
+{ 
+
+    printf("Hello my world!\n"); 
+
+    printf("MAGIC!\n"); 
+
+    return 0; 
+
+} 
+>>>>>>> 7529989 (lab01 submission)

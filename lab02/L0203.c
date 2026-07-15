29781376cba76fa6bc3e0b19bc6c0f234d09d038
SXNSXI <thansarut.ch@gmail.com>
2026-07-15 09:37:21 +0700

    lab02 submission

--- lab02/L0203.c
+++ lab02/L0203.c
@@ -2,25 +2,24 @@
 #include <stdlib.h>
 
 int main(){
-    int id;
+    int prod_ID;
     float price;
     char grade;
 
     printf("Enter Product ID : ");
-    scanf("%d",&id);
+    scanf("%d",&prod_ID);
 
     printf("Enter Price      : ");
     scanf("%f",&price);
 
     printf("Enter Grade      : ");
-    getchar(); // to consume the newline character
-    scanf("%c",&grade);
+    scanf(" %c",&grade);
 
     printf("===============================\n");
     printf("\tPRODUCT RECEIPT\n");
     printf("===============================\n");
 
-    printf("Product ID : |%08d|\n",id);
+    printf("Product ID : |%08d|\n",prod_ID);
     printf("Price      : |%8.2f|\n",price);
     printf("Grade      : |%-8c|\n",grade);
 

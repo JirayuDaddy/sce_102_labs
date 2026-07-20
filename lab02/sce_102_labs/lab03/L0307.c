97e6b17dc62167b4460b7e451d14c737ea3d6954
2611710183-blip <2611710183@tni.ac.th>
2026-07-20 21:23:21 +0700

    fix nested repo and update files

---
+++ lab02/sce_102_labs/lab03/L0307.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x1, x2, x3, x4;
+    float rms;
+    printf("Enter x1: ");
+    scanf("%f", &x1);
+    printf("Enter x2: ");
+    scanf("%f", &x2);
+    printf("Enter x3: ");
+    scanf("%f", &x3);
+    printf("Enter x4: ");
+    scanf("%f", &x4);
+    float sum_of_squares = pow(x1,2)+pow(x2,2)+pow(x3,2)+pow(x4,2);
+    rms = sqrt(sum_of_squares/4.0);
+    printf("\nxrms = %.3f\n",rms);
+    return 0;
+}

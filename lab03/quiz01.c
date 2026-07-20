e8e4f8ebb2645577715d9933adde870beeff3545
tsumiya13 <2611740016@tni.ac.th>
2026-07-20 10:40:56 +0700

    lab03 submission

---
+++ lab03/quiz01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int code,quantity;
+    char character;
+    float price;
+
+    printf("Enter product code: ");
+    scanf("%d",&code);
+    printf("Enter product initial (1 character): ");
+    scanf(" %c", &character);
+    printf("Enter product price: ");
+    scanf("%f", &price);
+    printf("Enter quantity: ");
+    scanf("%d", &quantity);
+
+    printf("\nCode  |Name  |Price    |Qty\n");
+    printf("  %d    %c      %.2f     %d\n",code,character,price,quantity);
+    printf("Total price: %.2f baht",price*quantity);
+    return 0;
+}

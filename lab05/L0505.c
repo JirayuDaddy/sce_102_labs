885dfc1eedf6444b21bcc92562492042158b1121
Your Name <you@example.com>
2026-08-03 12:07:26 +0700

    lab05 submission

--- lab05/L0505.c
+++ lab05/L0505.c
@@ -2,22 +2,49 @@
 #include <stdlib.h> 
 
 int main() {
-        int number,n=1;
-        float average;
+        int score,Highest_score,low_score,student_Highest_score,student_low_score;
+        int student=0;
+        float average=0.00;
 
-        printf("Student scrore calculator\n");
-        do { 
-        printf("Enter scrore for student %d : ",n,n+1);
-        n++ ;
-        scanf("%d",&number);
-        }
-        while (number > 0) ;
-        printf("\n");
-
-        average = number/n
-        printf("Average score :: %.2f ",average);
-
+        printf("Student score calculator\n ");
         
+        score = 0 ;
 
+        while(score >= 0)
+        {
+            printf("Enter score for student %d : ",student+1);
+            scanf("%d",&score);
+
+            if(score < 0){
+            break;
+            }
+            student++;
+            average = average + score ;
+
+            if(student == 1)
+            {
+            Highest_score = score ;
+            low_score = score ;
+            student_Highest_score = 1 ;
+            student_low_score =1 ;
+            }
+            if(score > Highest_score) {
+                    Highest_score = score ;
+                    student_Highest_score = student ;
+            }
+            if(score < low_score) {
+                    low_score = score ;
+                    student_low_score = student ;
+            }
+        
+        }
+
+        if(student > 0) {
+        average = average / student; 
+        }   
+        printf("Average score :: %.2f \n",average);
+        printf("Hight score   :: %d ,by student %d \n",Highest_score,student_Highest_score);
+        printf("Lowest score  :: %d ,by student %d",low_score,student_low_score);
+        
     return 0 ;
 }

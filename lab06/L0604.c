0117087e5bca9d0d87c93f34d57a109666136459
2611710183-blip <2611710183@tni.ac.th>
2026-08-23 13:23:13 +0700

    lab06 submission

---
+++ lab06/L0604.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int threeDices(){
+    int sc;
+    sc=rand()%16+3;
+    return sc;
+}
+int main(){
+    int x,y,z;
+    char b_ch, g_ch;
+    printf("Sic Bo Simulator\n\n");
+    printf("Enter wallet amount: ");
+    scanf("%d",x)
+    printf("place your bet: ");
+    printf("HIgh 'H' (11-18) or Low 'L' (3-10): ");
+    printf("HIgh 'H' (11-18) or Low 'L' (3-10): ");
+}

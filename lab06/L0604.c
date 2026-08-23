61766925f3dff18bc8e58424927419798819a1f0
2611710183-blip <2611710183@tni.ac.th>
2026-08-23 16:53:05 +0700

    Add files via upload

--- lab06/L0604.c
+++ lab06/L0604.c
@@ -6,13 +6,41 @@ int threeDices(){
     sc=rand()%16+3;
     return sc;
 }
-int main(){
-    int x,y,z;
+
+int main()
+{
+    int wallet, bet, sc;
     char b_ch, g_ch;
     printf("Sic Bo Simulator\n\n");
     printf("Enter wallet amount: ");
-    scanf("%d",x)
-    printf("place your bet: ");
-    printf("HIgh 'H' (11-18) or Low 'L' (3-10): ");
-    printf("HIgh 'H' (11-18) or Low 'L' (3-10): ");
+    scanf("%d",&wallet);
+    do{
+        printf("\nPlace your bet: ");
+        scanf("%d",&bet);
+        sc=threeDices();
+        do{
+            printf("High 'H' (11-18) or Low 'L' (3-10): ");
+            scanf(" %c",&b_ch);
+        }while(b_ch!='H'&&b_ch!='L');
+        printf("Three dices gave %d!\n", sc);
+        if(b_ch=='H'&&sc>=11){
+            printf("You win!\n");
+            wallet+=bet;
+        }
+        else if(b_ch=='L'&&sc<=10){
+            printf("You win!\n");
+            wallet+=bet;
+        }
+        else{
+            printf("You lose!\n");
+            wallet-=bet;
+        }
+        printf("\nCurrent wallet balance is %d\n\n",wallet);
+        do{
+            printf("Continue? (y/n): ");
+            scanf(" %c",&g_ch);
+        }while(g_ch!='y'&&g_ch!='n');
+    }while(g_ch!='n'&&wallet>0);
+    printf("\n\nGame over, please come again\n");
+    return 0;
 }

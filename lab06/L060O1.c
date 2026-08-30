8a6929b91921ac50ca91e4850aece8059b507b64
SXNSXI <thansarut.ch@gmail.com>
2026-08-30 09:27:33 +0700

    lab06 submission

--- lab06/L060O1.c
+++ lab06/L060O1.c
@@ -2,24 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-void Delay(int ms){
+void Delay(int ms)
+{
     clock_t start = clock();
     double elapsed;
-    do{
+    do
+    {
         elapsed = (double)(clock() - start) / CLOCKS_PER_SEC * 1000;
-    }while(elapsed < ms);
+    } while (elapsed < ms);
 }
 
-void fireCannon1(int n){
+void fireCannon1(int n)
+{
     int i, j;
-    for(i = 1; i <= n; i++){
+    for (i = 1; i <= n; i++)
+    {
         printf("\n");
         printf("Firing cannon 1 %d/%d\n", i, n);
 
-        if(i != n){
-            for(j = 1; j <= 3; j++){
+        if (i != n)
+        {
+            for (j = 1; j <= 3; j++)
+            {
                 Delay(2000);
-                printf("%d sec. passed\n", j*2);
+                printf("%d sec. passed\n", j * 2);
             }
         }
     }
@@ -27,16 +33,20 @@ void fireCannon1(int n){
     printf("Cannon 1 out of ammo\n");
 }
 
-void fireCannon2(int n){
+void fireCannon2(int n)
+{
     int i, j;
-    for(i = 1; i <= n; i++){
+    for (i = 1; i <= n; i++)
+    {
         printf("\n");
         printf("Firing cannon 2 %d/%d\n", i, n);
 
-        if(i != n){
-            for(j = 1; j <= 3; j++){
+        if (i != n)
+        {
+            for (j = 1; j <= 3; j++)
+            {
                 Delay(200);
-                printf("%.1f sec. passed\n", j*0.2);
+                printf("%.1f sec. passed\n", j * 0.2);
             }
         }
     }
@@ -44,7 +54,8 @@ void fireCannon2(int n){
     printf("Cannon 2 out of ammo\n");
 }
 
-int main(){
+int main()
+{
     int r1, r2;
 
     printf("Enter amount of rounds for cannon 1: ");

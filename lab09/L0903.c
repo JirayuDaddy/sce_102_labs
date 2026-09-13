e33f1c9e83364571c669b5840ec00dcfabd20aef
tsumiya13 <2611740016@tni.ac.th>
2026-09-13 23:08:41 +0700

    update lab09

--- lab09/L0903.c
+++ lab09/L0903.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 float speedDistance(float speed, int *time);
 float speedDistance(float speed, int *time)
 {
@@ -24,22 +25,3 @@ int main()
    return 0;
 }
 
-
-#include <stdio.h>
-int main()
-{
-   int num[12] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-   int *ptr;
-   ptr = num;
-   printf("original ::: ");
-   for(int i = 0; i < 12; i++)
-   {
-       printf("%d ", *(ptr + i));
-   }
-   printf("\nmultiplied ::: ");
-   for(int i = 0; i < 12; i++)
-   {
-       printf("%d ", *(ptr + i) * 24);
-   }
-   return 0;
-}

9843bcbc6a460818a56d8d0067519237c1bda37d
2611710142 <2611710142@tni.ac.th>
2026-09-14 11:52:40 +0700

    dq2

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,33 +1,15 @@
-#include <stdio.h>
-
-float avgheight(float height[], int i)
+int main ()
 {
-    float sum = 0;
-    for(int number = 0 ; number < i ; number++)
-    {
-        sum += height[number];
-    }
-    return sum / i;
-}
-
-int main()
-{
-    float height[30];
-    int i = 0;
-
-    for(i = 0 ; i < 30 ; i++)
-    {
-        printf("Enter height for students #%d: ", i + 1);
-        scanf("%f", &height[i]);
-        
-        if(height[i] <= 0 )
+do
         {
-            i--;
-            break;
+            printf("\nRoll dice?(y): ");
+            scanf(" %c", &choice);
+
+            if(choice != 'y')
+            {
+                printf("Error input\n");
+            }
         }
+        while(choice != 'y');
+return 0;
     }
-    float avg = avg_height(height , i)
-    printf("Average height is %.2f", avg);
-    printf("Number of students is %d", i);
-    return 0;
-}

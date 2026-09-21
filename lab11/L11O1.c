dbb497abcf85aee8c34be9d83b4c0a79efed1e6a
2611710118 <2611710118@tni.ac.th>
2026-09-21 10:27:16 +0700

    lab11 submission

--- lab11/L11O1.c
+++ lab11/L11O1.c
@@ -8,32 +8,39 @@ struct card
     char symbol;
 };
 
-void Drawingcard(struct card handCards[5],struct card new_card[5],int i)
-{
-    srand(time(NULL));
-    int card_point = handCards[i].point;
-    int card_symbol = handCards[i].symbol;
-    new_card[i].point = card_point%13 + 1;
-    new_card[i].symbol = card_symbol/13 + 65;
-    printf("%d%c ",new_card[i].point,new_card[i].symbol);
-}
+void Drawingcard(struct card handCards[5],struct card new_card[5],int i);
 
 int main()
 {
     struct card handCards[5];
+    srand(time(NULL));
     char choice;
     do
     {
-        printf("Draw 5 cards? (y/n) : ");
+        printf("\nDraw 5 cards? (y/n) : ");
         scanf(" %c",&choice);
         if(choice == 'y')
         {
-            struct card new_card[5];
-            printf("You got ");
+            printf("Yous got ");
             for(int i = 0 ; i<=4 ; i++)
-            {
-                Drawingcard(handCards,new_card,i);
+            {   
+                struct card new_card[5];
+                Drawingcard(handCards,new_card,i);   
             }
-    }
-    }while (choice!='y' && choice != 'n');
+            continue;
+        }
+
+    }while (choice != 'n');
+    return 0;
+}
+
+void Drawingcard(struct card handCards[5],struct card new_card[5],int i)
+{
+    int card_point = rand() % 13 + 1;
+    int card_symbol = rand() % 4 + 65;
+    new_card[i].point = card_point;
+    new_card[i].symbol = card_symbol;
+    handCards[i].point = new_card[i].point;
+    handCards[i].symbol = new_card[i].symbol;
+    printf("%d%c ",handCards[i].point,handCards[i].symbol);
 }
